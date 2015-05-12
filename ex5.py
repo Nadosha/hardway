821d62e8181c82f4aca1715dd8a6690ac9812f7d
Nadosha <andrey.nadosha@gmail.com>
2015-05-11 22:24:45 -0700

    Exercise 9

---
+++ ex5.py
@@ -0,0 +1,40 @@
+my_name = 'Zed A. Shaw'
+my_age = 26
+my_height = 174
+my_weight = 83
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "HE's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+#Stady drill
+print "=" * 10 + "Stady Drill" + "=" * 10
+
+name = 'Zed A. Shaw'
+age = 26
+height = 174
+weight = 83
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "HE's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %r eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+	age, height, weight, age + height + weight)
+
+print "=" * 10 + "Done!" + "=" * 10
